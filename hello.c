8835ba6c721a4f38a17c121981f63af76764f93a
Abhinav <abhinavkumar311997@gmail.com>
2018-03-05 06:51:40 +0530

    thirteeth

--- hello.c
+++ hello.c
@@ -6,7 +6,8 @@ void main(){
   printf("This must be a monolithic design\n");
   microkernel_sendmsg("is more portable");
 }
-
+/*This is the last change done to complete
+  the 30th step of the assignment*/
 void microkernel_sendmsg(char *a){
   printf("microkernel: %s\n", a);
 }
